a17a621aa2e88cc3d8d2be505385c36e70fbdbc9
RYOTA-EAST <70444118+RYOTA-EAST@users.noreply.github.com>
2021-01-19 20:23:10 +0900

    rubocop実行

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   with_options presence: true do
-    validates :nickname 
+    validates :nickname
     validates :birth_day
   end
 
