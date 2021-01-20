1e3afe18347b672c1043031aabf11c0dc1556bd1
RYOTA-EAST <70444118+RYOTA-EAST@users.noreply.github.com>
2021-01-20 22:21:47 +0900

    ActiveStorage導入
    出品した商品を一覧で表示する様編集したが別実装のためコメント化

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -9,11 +9,11 @@ class ItemsController < ApplicationController
   end
 
   def create
-
+    @item.save(item_params)
   end
 
   private
-  def method_name
-    
+  def item_params
+    params.requier(:message).pirmit(:name, :description, :category_id, :state_id, :ship_pay_id, :prefecture_id, :ship_day_id, :price, :image).merge(user_id:current_user.id)
   end
 end
