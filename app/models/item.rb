2a1b4ea37c53f74d32f8c503796b38776367de96
RYOTA-EAST <70444118+RYOTA-EAST@users.noreply.github.com>
2021-01-22 21:00:52 +0900

    価格のバリデーション

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,15 +1,32 @@
 class Item < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :state
+  belongs_to :ship_pay
+  belongs_to :prefecture
+  belongs_to :ship_day
+
   belongs_to :user
   has_one :order
   has_one_attached :image
 
-  with_options presence: false do
+  with_options presence: true do
     validates :name
     validates :description
+    validates :user
+    validates :image
+  end
+  with_options numericality: { other_than: 1 } do
     validates :category_id
     validates :state_id
+    validates :ship_pay_id
+    validates :prefecture_id
     validates :ship_day_id
-    validates :price
-    validates :user
   end
+    validates :price,
+    :numericality => {
+      :greater_than_or_equal_to => 300,
+      :less_than_or_equal_to => 9999999,
+      :message => '入力値が300~9999999の範囲外です' 
+    }
 end
