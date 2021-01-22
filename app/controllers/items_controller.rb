d03564db8624e5183cd8410a238ba53d06b0b0d8
RYOTA-EAST <70444118+RYOTA-EAST@users.noreply.github.com>
2021-01-22 21:01:47 +0900

    ログインしていないで出品ボタンを押すとログイン画面に遷移

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -5,15 +5,21 @@ class ItemsController < ApplicationController
   end
 
   def new
+    authenticate_user!
     @item = Item.new
   end
 
   def create
-    @item.save(item_params)
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new
+    end
   end
 
   private
   def item_params
-    params.requier(:message).pirmit(:name, :description, :category_id, :state_id, :ship_pay_id, :prefecture_id, :ship_day_id, :price, :image).merge(user_id:current_user.id)
+    params.require(:item).permit(:name, :description, :category_id, :state_id, :ship_pay_id, :prefecture_id, :ship_day_id, :price, :image).merge(user_id:current_user.id)
   end
 end
