2f400eb4cd98d279bdbb22d0e35af79c5ac4cd6b
RYOTA-EAST <70444118+RYOTA-EAST@users.noreply.github.com>
2021-01-24 20:47:58 +0900

    重複部分の修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -50,7 +50,6 @@ class ItemsController < ApplicationController
   end
 
   def move_to_index
-    item = Item.find(params[:id])
-    redirect_to root_path unless current_user == item.user
+    redirect_to root_path unless current_user == @item.user
   end
 end
