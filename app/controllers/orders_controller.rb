369a4c0e4b208c8b1d53896baeea45ad040b57f1
RYOTA-EAST <70444118+RYOTA-EAST@users.noreply.github.com>
2021-01-30 19:06:09 +0900

    商品購入実装（住所登録のみ）

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,5 +1,30 @@
 class OrdersController < ApplicationController
   def index
     @item = Item.find(params[:item_id])
+    @item_order = ItemOrder.new
+    #form_wirh繋がってる
   end
+
+  def new
+    #ここにあった.newをindex
+  end
+
+  def create
+      @item = Item.find(params[:item_id])
+      @item_order = ItemOrder.new(order_params)
+      if @item_order.valid?
+      @item_order.save
+      redirect_to root_path
+    else
+      render action: :index
+    end
+  end
+
+  private
+  def order_params
+    params.require(:item_order).permit(:postal_code, :prefecture_id, :city, :address, :explosive, :phone_number).merge(user_id: current_user.id, item_id:@item.id)
+  #requireがない
+  #idを結合させた
+  end
+
 end
---
+++ app/models/item_order.rb
@@ -0,0 +1,20 @@
+class ItemOrder
+  include ActiveModel::Model
+  attr_accessor :postal_code, :prefecture_id, :city, :address, :explosive, :phone_number, :user_id, :item_id
+  #この中で作られたものは不要（外から来たのを指定）
+
+  with_options presence: true do
+    validates :postal_code, format: { with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)" }
+    validates :city
+    validates :address
+    validates :phone_number, format: { with: /\A\d{11}\z/, message: "電話番号は１１桁以内です" }
+  end
+
+  validates :prefecture_id, numericality: { other_than:1 ,message: "can't be blank" }
+
+  def save
+    order = Order.create(item_id: item_id, user_id: user_id)
+    #orderに入れて下でidを追加
+    ShipAddress.create(postal_code: postal_code,prefecture_id: prefecture_id,city: city,address: address,explosive: explosive,phone_number: phone_number, order_id: order.id)
+  end
+end
